06839817e2caa1b2fb428f2d8bc499182024572e
phsaikiran <saikiran.2310@gmail.com>
2023-10-28 03:22:04 -0500

    Nearby grid only check

--- game.py
+++ game.py
@@ -37,22 +37,43 @@ def start_simulation():
 
         # Check for collisions
         for i in range(Const.TOTAL_BALLS):
-            for j in range(i + 1, Const.TOTAL_BALLS):
+            # Get all points in the same grid as point i
+            grid_index = inverse_points_grid[i]
+            nearby_points = points_grid[grid_index].copy()
+            # Get points from neighboring grids
+            for nearby_grid_offset_x in range(-1, 2):
+                for nearby_grid_offset_y in range(-1, 2):
+                    if (grid_index[0] + nearby_grid_offset_x < 0 or
+                            grid_index[0] + nearby_grid_offset_x >= Const.GRID_RESOLUTION or
+                            grid_index[1] + nearby_grid_offset_y < 0 or
+                            grid_index[1] + nearby_grid_offset_y >= Const.GRID_RESOLUTION or
+                            (nearby_grid_offset_x == 0 and nearby_grid_offset_y == 0)):
+                        continue
+                    new_grid_index = (grid_index[0] + nearby_grid_offset_x, grid_index[1] + nearby_grid_offset_y)
+                    nearby_points.extend(points_grid[new_grid_index].copy())
+
+            for other_point_index in nearby_points:
+                if i == other_point_index:
+                    continue
+
                 point = points[i]
-                other_point = points[j]
+                other_point = points[other_point_index]
 
                 diff = point.pos - other_point.pos
+                if diff.length() == 0:
+                    print("ERROR: diff.length() == 0")
+                    continue
                 if diff.length() <= Const.BALL_SIZE * 2:
                     # If balls are overlapping, move them apart
                     point.pos += diff.normalize() * (Const.BALL_SIZE * 2 - diff.length())
                     other_point.pos -= diff.normalize() * (Const.BALL_SIZE * 2 - diff.length())
                     # Elastic collision between two balls with equal mass
-                    point.vel = point.vel.reflect(diff.normalize())
-                    other_point.vel = other_point.vel.reflect(diff.normalize())
+                    point.vel = point.vel.reflect(diff.normalize()) * 0.9
+                    other_point.vel = other_point.vel.reflect(diff.normalize()) * 0.9
 
         for index, point in points.items():
             # Update velocity based on gravity
-            # point.vel.y += 1000 * dt
+            point.vel.y += 1000 * dt
             point.pos += point.vel * dt
 
             # Check if point is out of bounds
@@ -65,6 +86,13 @@ def start_simulation():
                 point.vel.x *= -1 * 0.9
             if point.pos.y <= Const.CHECK_Y_MIN or point.pos.y >= Const.CHECK_Y_MAX:
                 point.vel.y *= -1 * 0.9
+
+            # Update grid
+            grid_x = int(point.pos.x / Const.X_SCREEN_SIZE * Const.GRID_RESOLUTION)
+            grid_y = int(point.pos.y / Const.Y_SCREEN_SIZE * Const.GRID_RESOLUTION)
+            points_grid[inverse_points_grid[index]].remove(index)
+            inverse_points_grid[index] = (grid_x, grid_y)
+            points_grid[inverse_points_grid[index]].append(index)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
